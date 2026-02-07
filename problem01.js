524fe87ae8c8c441a95ea46949ad185511207fbf
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-02-07 12:53:24 +0600

    add problem 02

--- problem01.js
+++ problem01.js
@@ -2,9 +2,45 @@
 
 function newPrice(currentPrice, discount) {
     // Your code here
-    const discountMoney = currentPrice*discount/100;
+        if (typeof currentPrice !== 'number' || typeof discount !== 'number' || discount<0 || discount > 100) {
+        console.log("Invalid");
+        return 'Invalid';
+    }
+    // if (discount < 0 || discount > 100 ){
+    //     console.log("Invalid");
+    //     return 'Invalid';
+    // }
+    const discountMoney = currentPrice * discount/100;
     console.log("discount: ",discountMoney);
     const finalPayment = currentPrice -discountMoney;
-    console.log("final Payment: " ,finalPayment);
+    console.log("final Payment: " ,finalPayment.toFixed(3));
+    return finalPayment;
 }
-newPrice(1500,20);
+console.log('-----------');
+// newPrice(1500,20);
+// newPrice(2000,15);
+// newPrice(1200, 7);
+// newPrice("1000", 10);
+// newPrice(2000, 17.17);
+// console.log("-----------");
+newPrice(500, "5");
+newPrice(500,100.1);
+// newPrice(500, -3);
+// newPrice(500,0);
+
+
+
+// ----------------
+
+// function newPrice(currentPrice, discount) {
+//    if (typeof currentPrice !== 'number' || typeof discount !== 'number' || discount<0 || discount > 100){
+//         // console.log("invalid");
+//         return 'Invalid';
+//     }
+//     const discountMoney = currentPrice*discount/100;
+//     // console.log("discount: ",discountMoney);
+//     const finalPayment = currentPrice -discountMoney;
+//     // console.log(finalPayment.toFixed(3));
+//     return finalPayment.toFixed(3);
+
+// }
---
+++ problem02.js
@@ -0,0 +1,3 @@
+function validOtp(otp) {
+    // Your code here
+}
