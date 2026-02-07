409815d1484ac5eb6eba5194f40d288eff765edf
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-02-07 18:02:21 +0600

    problem02 added

--- problem02.js
+++ problem02.js
@@ -1,3 +1,18 @@
+
+
 function validOtp(otp) {
     // Your code here
+    if (typeof otp !== 'string'){
+        return "Invalid";
+    }
+    if(otp.length !== 8){
+        return false;
+    } else{
+       return true;
+    }
+  
 }
+  const output = validOtp ('1212121222');
+  console.log(output);
+//   console.log(validOtp(1111111222222));
+// validOtp(111111111);
