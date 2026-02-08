7458d81eb7caf9eb427912f5a8187bccbbba91d8
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-02-08 12:54:52 +0600

    problem05

---
+++ Problem05.js
@@ -0,0 +1,28 @@
+//  problem solve : 05
+
+function analyzeText(str) {
+    // Your code here
+    if(typeof str !== 'string'){
+        return "Invalid";
+    }
+    if(str ===""){
+        return "Invalid";
+    }
+    let words = str.split(" ");
+    let longWord = "";
+    let token = str.split(" ").join("");
+    
+    for (let word of words){
+        if (word.length>longWord.length){
+            longWord = word;
+    }
+    }
+    return {
+        longWords: longWord,
+        token: token.length
+    }
+ 
+}
+
+console.log(analyzeText("I am a little honest person"));
+// console.log(analyzeText(""));
